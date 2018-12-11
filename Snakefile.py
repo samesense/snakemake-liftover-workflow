2ff9e3de96feb0835291c8548c3a1dab48763f14
Perry <samesense@users.noreply.github.com>
2018-12-11 11:38:14 -0500

    Update Snakefile.py

--- Snakefile.py
+++ Snakefile.py
@@ -3,7 +3,7 @@ HTTP = HTTPRemoteProvider()
 
 rule dl_chain_file:
     input:  HTTP.remote('hgdownload.cse.ucsc.edu/goldenPath/{from}/liftOver/{from}To{to}.over.chain.gz', insecure=True, keep_local=True, allow_redirects=True)
-    output: {chain_prefix}ucsc/chains/{from}To{to}.over.chain.gz'
+    output: '{chain_prefix}ucsc/chains/{from}To{to}.over.chain.gz'
     shell:  'mv {input} {output}'
 
 rule lift_hapmap:
